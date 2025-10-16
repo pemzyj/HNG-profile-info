d833122558294ebd6b54d07c930ecdf0c1bddaf1
Olamide <pemzyj123@gmail.com>
2025-10-16 16:32:09 +0100

    completed the API endpoint

---
+++ index.js
@@ -0,0 +1,42 @@
+import express from 'express';
+
+
+const app = express();
+
+
+
+const user = {
+    email: 'pemzyj123@gmail.com',
+    name: 'Olamide Adeosun',
+    stack: 'Node.js/Express, MongoDB, PostgreSQL'
+};
+
+
+
+async function fetchCatFact () {
+    try{
+        const res = await fetch('https://catfact.ninja/fact', {timeout: 5000});
+        if (!res.ok) {
+            throw new Error(`API error: ${res.status}`); //throw new Error provides a message
+        }
+        const data = await res.json();
+        return data;
+    } catch (error) {
+        console.error(error.message);  // error.message is dependent on the error type
+        // fallback message for API failure
+        return { error: 'API is currently unavailable, please try later.' };
+    };
+}
+
+
+app.get('/me', async (req, res) => {
+    let timestamp = new Date().toISOString();
+    const data = await fetchCatFact();
+    if (data.error) {
+        return res.status(503).json(data);
+    }
+    res.status(200).json({status: 'success', user:user, timestamp:timestamp, fact:data}); 
+});
+
+
+app.listen(8080, ()=>console.log('server is running on http://localhost:8080'));
